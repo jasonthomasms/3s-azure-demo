106dcaf4b79cd41040655d7c5836682f503ef99f
Aroon Nataraj <arnatara@microsoft.com>
2021-07-16 11:59:07 -0700

    integration

--- Models/SearchRequest.cs
+++ Models/SearchRequest.cs
@@ -12,17 +12,21 @@ namespace AzureIntegrationDemo.Models
 
         public string Id { get; set; }
 
-        public string Headers { get; set; }
         public string Body { get; set; }
 
         public string Parameters { get; set; }
 
-        public SearchRequest(string headers, string body, string parameters)
+        public string Headers { get; set; }
+        
+        public string Token { get; set; }
+
+        public SearchRequest(string headers, string body, string parameters, string token)
         {
             this.Id = Guid.NewGuid().ToString();
             this.Headers = headers;
             this.Body = body;
             this.Parameters = parameters;
+            this.Token = token;
         }
     }
 }
